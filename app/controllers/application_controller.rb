6683d64bba2b03a7c8b90d2d9ca5f6030dececd9
Jonathan Buckland <funcoder@gmail.com>
2025-10-20 16:52:15 +0100

    Simplify allow_browser comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  # Allow browsers - use permissive settings for compatibility
-  # Disabled temporarily due to 406 issues
-  # allow_browser versions: { safari: "12", chrome: "80", firefox: "78", edge: "80" }
+  # Commenting out allow_browser to prevent 406 errors
+  # allow_browser versions: :modern
 end
