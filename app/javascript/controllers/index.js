2527465fb09c4951730602e0f2bbdb8a5e0decd4
Jonathan Buckland <funcoder@gmail.com>
2025-11-06 17:36:45 +0000

    Fix Stimulus controller loading by using automatic registration
    - Switch from manual controller imports to @hotwired/stimulus-loading
    - Use eagerLoadControllersFrom() for automatic controller discovery
    - This fixes 404 errors when trying to load individual controllers
    - Follows Rails 8 standard approach for Stimulus with importmap

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,47 +2,7 @@
 // Controller files must be named *_controller.js.
 
 import { application } from "controllers/application"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-// Brain Dump
-import BrainDumpController from "controllers/brain_dump_controller.js"
-application.register("brain-dump", BrainDumpController)
-
-// Cookie Consent
-import CookieConsentController from "controllers/cookie_consent_controller.js"
-application.register("cookie-consent", CookieConsentController)
-
-// Dropdown
-import DropdownController from "controllers/dropdown_controller.js"
-application.register("dropdown", DropdownController)
-
-// Hello
-import HelloController from "controllers/hello_controller.js"
-application.register("hello", HelloController)
-
-// Mobile Menu
-import MobileMenuController from "controllers/mobile_menu_controller.js"
-application.register("mobile-menu", MobileMenuController)
-
-// Notifications
-import NotificationsController from "controllers/notifications_controller.js"
-application.register("notifications", NotificationsController)
-
-// Pomodoro
-import PomodoroController from "controllers/pomodoro_controller.js"
-application.register("pomodoro", PomodoroController)
-
-// PWA Install
-import PwaInstallController from "controllers/pwa_install_controller.js"
-application.register("pwa-install", PwaInstallController)
-
-// Service Worker
-import ServiceWorkerController from "controllers/service_worker_controller.js"
-application.register("service-worker", ServiceWorkerController)
-
-// Task
-import TaskController from "controllers/task_controller.js"
-application.register("task", TaskController)
-
-// User Menu
-import UserMenuController from "controllers/user_menu_controller.js"
-application.register("user-menu", UserMenuController)
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+eagerLoadControllersFrom("controllers", application)
